1bfbe04e20b528c1eaf0628542e6f0a0232629a7
ANAMARIA COPTIL <anamaria.coptil@stud.ubbcluj.ro>
2020-11-25 21:22:45 +0200

    Add project files.

---
+++ Data/Coptil_Anamaria_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Coptil_Anamaria_Lab8.Models;
+
+namespace Coptil_Anamaria_Lab8.Data
+{
+    public class Coptil_Anamaria_Lab8Context : DbContext
+    {
+        public Coptil_Anamaria_Lab8Context (DbContextOptions<Coptil_Anamaria_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coptil_Anamaria_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Coptil_Anamaria_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Coptil_Anamaria_Lab8.Models.Category> Category { get; set; }
+    }
+}
